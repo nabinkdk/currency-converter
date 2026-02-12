4d69ec1578810cb306a2dfbc651dfa52fa46cdac
Nabin Khadka <khadkanabin090@gmail.com>
2026-02-12 08:31:16 +0000

    created a user input function where it takes user input and return the input where in main function it handles the user input if there is any error or not!

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-def main():
-    pass
+def user_input():
+    base_curr = input("Enter a base currency(Eg: USD, GBP): ").upper()
+    target_curr = input("Enter a base currency(Eg: NPR, INR): ").upper()
+    return base_curr, target_curr
 
-if __name__=="main":
+
+def main():
+    while True:
+        base_curr, target_curr = user_input()
+
+        if not base_curr.isalpha() or not target_curr.isalpha():
+            print("Currency must contain letters only!")
+            continue
+
+        if len(base_curr) != 3 or len(target_curr) != 3:
+            print("Currency code must contain exactly 3 letters!")
+            continue
+        break
+
+if __name__ == "__main__":
     main()
+
+   
